7ae92ec7e6206318a31caa2d4d890fa8774db57c
Harsh V Srivastava <harshvsri@gmail.com>
2023-07-13 00:21:25 +0530

    Add Display.tsx

---
+++ src/components/Display.tsx
@@ -0,0 +1,12 @@
+function Display() {
+  return (
+    <>
+      <div className="container__display">
+        <h1>We are under maintenance. 😎</h1>
+        <h2>Will be right back soon. Hang tight! 😉</h2>
+      </div>
+    </>
+  );
+}
+
+export default Display;
